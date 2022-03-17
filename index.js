4fa6124d5355ed90b60be13832fc45f6ea5f826f
enti <enti@enti.enti>
2022-03-17 20:22:32 +0100

    Ejercicio dia 17

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
 #!/usr/bin/node
- 
-let http = require("http");
-let fs = require("fs");
 
+let http = require("http");
 let mongo_client = require("mongodb").MongoClient;
 
 let url = "mongodb://localhost/";
 
 let db;
+let fs = require("fs");
 
-console.log("Iniciando script mongo-http");
+console.log("Iniciando Script mongo_http");
 
-mongo_client.connect(url, function(error, conn){
-    console.log("Dentro de MongoDB");
-
-    if (error){
-        console.log("ERROR!!!");
-        return;
-    }
-
-    db = conn.db("tffhd");
-
+mongo_client.connect(url, function(error,conn){
+	console.log("Dentro de Mongo");
+	if (error){
+		console.log("Esto no furula TwT");
+		return;
+	}
+	db = conn.db("tffhd");
 });
 
- 
-http.createServer(function(request, response){
-    response.writeHead(200);
-	
-	if (request.url == "/"){
-		fs.readFile("index.html", function (err, data){
-			response.writeHead(200, {"Content-Type": "text/html"});
-			response.end(data);
+http.createServer(function(req, res) {
+	res.writeHead(200);
+	let col = "";
 
+	if (req.url == "/") {
+		fs.readFile("index.html",function(err, data){
+			res.writeHead(200, {"Content-Type":"text/html"});
+			res.end(data);
 		});
-		
 		return;
 	}
+	
 
-	let col = "";
-	 
-	if (request.url == "/characters"){
+	if (req.url == "/characters") {
 		col = "characters";
-	} 
-	else if (request.url == "/items"){
+	}
+	else if (req.url == "/items") {
 		col = "items";
 	}
-	else {
-		response.end();
-		return;
-	}
+	else { return; }
 
 	let col_data = db.collection(col).find();
-
-	col_data.toArray(function(err, data){
+	
+	col_data.toArray(function(err, data) {
 		let string = JSON.stringify(data);
-	response.end(string);
+		
+		res.end(string);
 	});
 
-	
-}).listen(1900);
-
- 
+}).listen(1909);
