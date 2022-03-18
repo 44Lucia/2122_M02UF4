32ed2bac6ab50dd482988d30d566fc5cf0be276f
enti <enti@enti.enti>
2022-03-18 11:57:14 +0100

    Clase dia 18

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 let http = require("http");
 let mongo_client = require("mongodb").MongoClient;
+let ObjectId = require("mongodb").ObjectID;
 
 let url = "mongodb://localhost/";
 
@@ -19,9 +20,29 @@ mongo_client.connect(url, function(error,conn){
 	db = conn.db("tffhd");
 });
 
+function send_data_list(req ,res)
+{
+	let col= "";
+
+	if (req.url == "/characters") {
+    	col = "characters";
+    }
+    else if (req.url == "/items") {
+        col = "items";
+    }
+    else { res.end(); return; }
+
+    let col_data = db.collection(col).find({},{projection: { name:1 } });
+
+    col_data.toArray(function(err, data) {
+    	let string = JSON.stringify(data);
+
+         res.end(string);
+     });
+}
+
 http.createServer(function(req, res) {
 	res.writeHead(200);
-	let col = "";
 
 	if (req.url == "/") {
 		fs.readFile("index.html",function(err, data){
@@ -30,22 +51,32 @@ http.createServer(function(req, res) {
 		});
 		return;
 	}
-	
 
-	if (req.url == "/characters") {
-		col = "characters";
-	}
-	else if (req.url == "/items") {
-		col = "items";
-	}
-	else { return; }
-
-	let col_data = db.collection(col).find();
+	let url = req.url.split("/");
+	console.log(url);
 	
-	col_data.toArray(function(err, data) {
-		let string = JSON.stringify(data);
+	if (url.length == 2){
+		send_data_list(req,res);
+		return;
+	}
+	else{
+		if (url[2].length != 24){
+			res.end();
+			return;
+		}
+		if(url[1] == "characters"){
+			let obj_id = new ObjectId(url[2]);
+			let col_data = db.collection("characters").find({"_id":obj_id});
 		
-		res.end(string);
-	});
+			col_data.toArray(function(err, data) {
+	         	let string = JSON.stringify(data);
+
+    	      	res.end(string);
+     		});
+		}
+		else if (url[1] == "items"){
+
+		}
+	}
 
 }).listen(1909);
